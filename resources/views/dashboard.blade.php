469682534f67c577c96c6270fa341c633a485626
Adly Ramdhani <adlyramdhani@Adlys-MacBook-Pro.local>
2025-11-06 06:01:26 +0700

    push

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    @vite(['resources/css/app.css'])
+    <link rel="stylesheet" href="resources/css/app.css'">
+    {{-- @vite(['resources/css/app.css']) --}}
     <style>
       /* Force About section visible on small devices and prevent hero from covering it */
       @media (max-width: 991.98px) {
