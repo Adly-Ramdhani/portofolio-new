8342233ec21bbfd0253bdda994cfb95b189b0fa0
Adly Ramdhani <adlyramdhani@Adlys-MacBook-Pro.local>
2025-12-15 13:15:52 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,28 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 
-              'resources/js/app.js',
-              'resources/css/login.css',
-              'resources/css/admin.css'],
+      input: [
+        'resources/css/app.css', 
+        'resources/js/app.js',
+        'resources/css/login.css',
+        'resources/css/admin.css'
+      ],
       refresh: true,
       buildDirectory: 'build',
     }),
     tailwindcss(),
   ],
+
+  // ⬇️ INI WAJIB
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    https: false,
+  },
+
   build: {
-    outDir: 'public/build',  
-    manifest: true,          
-    emptyOutDir: true,       
+    outDir: 'public/build',
+    manifest: true,
+    emptyOutDir: true,
   },
 })
